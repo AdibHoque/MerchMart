7a9496d3f8dfbcb731cbed4b9b2937592a6459b0
AdibHoque <adibhoque04@gmail.com>
2024-08-17 17:24:01 +0600

    Added Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import Cards from "./components/Cards";
+import Footer from "./components/Footer";
 import Navbar from "./components/Navbar";
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
     <>
       <Navbar></Navbar>
       <Cards></Cards>
+      <Footer></Footer>
     </>
   );
 }
